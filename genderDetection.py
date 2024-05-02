fbb53b83e235544c9a0dee728edf9922cdb04896
viks12334 <168765477+viks12334@users.noreply.github.com>
2024-05-03 03:04:57 +0900

    Update genderDetection.py

--- genderDetection.py
+++ genderDetection.py
@@ -3,9 +3,9 @@ import numpy as np
 import tensorflow as tf
 import librosa
 
-st.title("Gender Detection from Audio")
-st.write("## Upload any piece of voice and this model will predict its gender")
-uploader = st.file_uploader("Upload a voice recording")
+st.title("Распознавание пола по аудио")
+st.write("## Загрузите аудиофайл для распознавания")
+uploader = st.file_uploader("Загрузить аудиозапись")
 model = tf.keras.models.load_model('model/kaggle/working/model')
 if uploader is not None:
     with open("audio.mp3","wb") as f:
@@ -15,8 +15,8 @@ if uploader is not None:
     b = tf.keras.utils.pad_sequences(mfcc_wf, padding='post', maxlen=200)
     pred = model.predict(np.array([b]))[0]
     if pred <= 0.65:
-        st.write(f"## Female with accuracy of {float((1-pred[0])*100).__round__(2)}%")
+        st.write(f"## Женский с точностью {float((1-pred[0])*100).__round__(2)}%")
     else:
-        st.write(f"## Male with accuracy of {float((pred[0])*100).__round__(2)}%")
+        st.write(f"## Мужской с точностью {float((pred[0])*100).__round__(2)}%")
 
 
